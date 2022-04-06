0afefaf1780f5b961808c95ae7a2b463aef24773
zxc <798938068@qq.com>
2022-04-06 09:23:01 -0700

    20220406

--- bomb/bomb.asm
+++ bomb/bomb.asm
@@ -804,15 +804,15 @@ Disassembly of section .text:
 000000000040145c <read_six_numbers>:
   40145c:	48 83 ec 18          	sub    $0x18,%rsp
   401460:	48 89 f2             	mov    %rsi,%rdx
-  401463:	48 8d 4e 04          	lea    0x4(%rsi),%rcx
-  401467:	48 8d 46 14          	lea    0x14(%rsi),%rax
-  40146b:	48 89 44 24 08       	mov    %rax,0x8(%rsp)
-  401470:	48 8d 46 10          	lea    0x10(%rsi),%rax
-  401474:	48 89 04 24          	mov    %rax,(%rsp)
-  401478:	4c 8d 4e 0c          	lea    0xc(%rsi),%r9
-  40147c:	4c 8d 46 08          	lea    0x8(%rsi),%r8
-  401480:	be c3 25 40 00       	mov    $0x4025c3,%esi
-  401485:	b8 00 00 00 00       	mov    $0x0,%eax
+  401463:	48 8d 4e 04          	lea    0x4(%rsi),%rcx  ;rcx=rsi-4
+  401467:	48 8d 46 14          	lea    0x14(%rsi),%rax ;rax=rsi-20
+  40146b:	48 89 44 24 08       	mov    %rax,0x8(%rsp)  ;*(rsp+8)=rsi-20
+  401470:	48 8d 46 10          	lea    0x10(%rsi),%rax ;rax=rsi+10
+  401474:	48 89 04 24          	mov    %rax,(%rsp)     ;*rsp=rsi+10
+  401478:	4c 8d 4e 0c          	lea    0xc(%rsi),%r9   ;r9=rsi+12
+  40147c:	4c 8d 46 08          	lea    0x8(%rsi),%r8   ;r8=rsi+8
+  401480:	be c3 25 40 00       	mov    $0x4025c3,%esi  ;esi=0x4025c3
+  401485:	b8 00 00 00 00       	mov    $0x0,%eax       ;eax=0
   40148a:	e8 61 f7 ff ff       	callq  400bf0 <__isoc99_sscanf@plt>
   40148f:	83 f8 05             	cmp    $0x5,%eax
   401492:	7f 05                	jg     401499 <read_six_numbers+0x3d>
