2079b8799feb373d42a17e50d3adf697286054cb
zxc <798938068@qq.com>
2022-04-07 01:41:58 -0700

    phase3 and phase4

--- bomb/bomb.asm
+++ bomb/bomb.asm
@@ -382,18 +382,19 @@ Disassembly of section .text:
 
 0000000000400f43 <phase_3>:
   400f43:	48 83 ec 18          	sub    $0x18,%rsp
-  400f47:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx
-  400f4c:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx
+  400f47:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx;rcx=rsp+12
+  400f4c:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx;rdx=rsp+8
   400f51:	be cf 25 40 00       	mov    $0x4025cf,%esi
   400f56:	b8 00 00 00 00       	mov    $0x0,%eax
   400f5b:	e8 90 fc ff ff       	callq  400bf0 <__isoc99_sscanf@plt>
-  400f60:	83 f8 01             	cmp    $0x1,%eax
+  400f60:	83 f8 01             	cmp    $0x1,%eax;num of input
   400f63:	7f 05                	jg     400f6a <phase_3+0x27>
   400f65:	e8 d0 04 00 00       	callq  40143a <explode_bomb>
-  400f6a:	83 7c 24 08 07       	cmpl   $0x7,0x8(%rsp)
-  400f6f:	77 3c                	ja     400fad <phase_3+0x6a>
-  400f71:	8b 44 24 08          	mov    0x8(%rsp),%eax
+  400f6a:	83 7c 24 08 07       	cmpl   $0x7,0x8(%rsp);rsp point to ipt
+  400f6f:	77 3c                	ja     400fad <phase_3+0x6a>;>7 or <0
+  400f71:	8b 44 24 08          	mov    0x8(%rsp),%eax ;eax=*(rsp+8)
   400f75:	ff 24 c5 70 24 40 00 	jmpq   *0x402470(,%rax,8)
+						;jmp 0x400f7c+8*(*rax)
   400f7c:	b8 cf 00 00 00       	mov    $0xcf,%eax
   400f81:	eb 3b                	jmp    400fbe <phase_3+0x7b>
   400f83:	b8 c3 02 00 00       	mov    $0x2c3,%eax
@@ -412,31 +413,33 @@ Disassembly of section .text:
   400fb2:	b8 00 00 00 00       	mov    $0x0,%eax
   400fb7:	eb 05                	jmp    400fbe <phase_3+0x7b>
   400fb9:	b8 37 01 00 00       	mov    $0x137,%eax
-  400fbe:	3b 44 24 0c          	cmp    0xc(%rsp),%eax
+  400fbe:	3b 44 24 0c          	cmp    0xc(%rsp),%eax;eax-*(rsp+12)
   400fc2:	74 05                	je     400fc9 <phase_3+0x86>
   400fc4:	e8 71 04 00 00       	callq  40143a <explode_bomb>
   400fc9:	48 83 c4 18          	add    $0x18,%rsp
   400fcd:	c3                   	retq   
 
-0000000000400fce <func4>:
+0000000000400fce <func4>:				 ;edx=14 esi=0 edi=ipt1
   400fce:	48 83 ec 08          	sub    $0x8,%rsp
   400fd2:	89 d0                	mov    %edx,%eax
-  400fd4:	29 f0                	sub    %esi,%eax
-  400fd6:	89 c1                	mov    %eax,%ecx
-  400fd8:	c1 e9 1f             	shr    $0x1f,%ecx
+  400fd4:	29 f0                	sub    %esi,%eax 
+  400fd6:	89 c1                	mov    %eax,%ecx ;ecx=edx-esi
+  400fd8:	c1 e9 1f             	shr    $0x1f,%ecx;ecx=1(edx<esi),0(>=)
   400fdb:	01 c8                	add    %ecx,%eax
-  400fdd:	d1 f8                	sar    %eax
-  400fdf:	8d 0c 30             	lea    (%rax,%rsi,1),%ecx
-  400fe2:	39 f9                	cmp    %edi,%ecx
+  400fdd:	d1 f8                	sar    %eax     	;eax/=2
+  400fdf:	8d 0c 30             	lea    (%rax,%rsi,1),%ecx;ecx=rax+rsi
+  400fe2:	39 f9                	cmp    %edi,%ecx        ;edi=1st input
   400fe4:	7e 0c                	jle    400ff2 <func4+0x24>
-  400fe6:	8d 51 ff             	lea    -0x1(%rcx),%edx
+					;1st input >7
+  400fe6:	8d 51 ff             	lea    -0x1(%rcx),%edx  ;edx=6
   400fe9:	e8 e0 ff ff ff       	callq  400fce <func4>
   400fee:	01 c0                	add    %eax,%eax
   400ff0:	eb 15                	jmp    401007 <func4+0x39>
-  400ff2:	b8 00 00 00 00       	mov    $0x0,%eax
-  400ff7:	39 f9                	cmp    %edi,%ecx
-  400ff9:	7d 0c                	jge    401007 <func4+0x39>
-  400ffb:	8d 71 01             	lea    0x1(%rcx),%esi
+					;1st input <=7
+  400ff2:	b8 00 00 00 00       	mov    $0x0,%eax  ;
+  400ff7:	39 f9                	cmp    %edi,%ecx  ;7-ipt1
+  400ff9:	7d 0c                	jge    401007 <func4+0x39>;ipt1<=7 ret
+  400ffb:	8d 71 01             	lea    0x1(%rcx),%esi;
   400ffe:	e8 cb ff ff ff       	callq  400fce <func4>
   401003:	8d 44 00 01          	lea    0x1(%rax,%rax,1),%eax
   401007:	48 83 c4 08          	add    $0x8,%rsp
@@ -451,7 +454,7 @@ Disassembly of section .text:
   401024:	e8 c7 fb ff ff       	callq  400bf0 <__isoc99_sscanf@plt>
   401029:	83 f8 02             	cmp    $0x2,%eax
   40102c:	75 07                	jne    401035 <phase_4+0x29>
-  40102e:	83 7c 24 08 0e       	cmpl   $0xe,0x8(%rsp)
+  40102e:	83 7c 24 08 0e       	cmpl   $0xe,0x8(%rsp);1st input <= 14
   401033:	76 05                	jbe    40103a <phase_4+0x2e>
   401035:	e8 00 04 00 00       	callq  40143a <explode_bomb>
   40103a:	ba 0e 00 00 00       	mov    $0xe,%edx
